2a5ec02c109fc15d20449128f01308dad128a5d0
dewall <a.dewall@52north.org>
2017-11-06 15:10:39 +0100

    added initial classes

---
+++ src/main/java/org/envirocar/processing/mapmatching/mmservice/MapMatcherConfiguration.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2017 the enviroCar community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.envirocar.processing.mapmatching.mmservice;
+
+import com.graphhopper.GraphHopper;
+import com.graphhopper.matching.MapMatching;
+import com.graphhopper.reader.osm.GraphHopperOSM;
+import com.graphhopper.routing.AlgorithmOptions;
+import com.graphhopper.routing.util.AbstractFlagEncoder;
+import com.graphhopper.routing.util.CarFlagEncoder;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.routing.weighting.FastestWeighting;
+import com.graphhopper.routing.weighting.Weighting;
+import com.graphhopper.util.Parameters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author dewall
+ */
+@Configuration
+public class MapMatcherConfiguration {
+
+    @Bean
+    public AbstractFlagEncoder provideFlagEncoder() {
+        return new CarFlagEncoder();
+    }
+
+    @Bean
+    public GraphHopper provideGraphHopper(AbstractFlagEncoder encoder) {
+        GraphHopper hopper = new GraphHopperOSM();
+        hopper.setDataReaderFile("./map-data/muenster-regbez-latest.osm.pbf");
+        hopper.setGraphHopperLocation("./target/mapmatchingtest");
+        hopper.setEncodingManager(new EncodingManager(encoder));
+        hopper.getCHFactoryDecorator().setEnabled(false);
+        hopper.importOrLoad();
+        return hopper;
+    }
+
+    @Bean
+    public MapMatching provideMapMatching(AbstractFlagEncoder encoder, GraphHopper hopper) {
+        String algorithm = Parameters.Algorithms.DIJKSTRA_BI;
+        Weighting weighting = new FastestWeighting(encoder);
+        AlgorithmOptions algoOptions = new AlgorithmOptions(algorithm, weighting);
+        MapMatching mapMatching = new MapMatching(hopper, algoOptions);
+        return mapMatching;
+    }
+}
---
+++ src/main/java/org/envirocar/processing/mapmatching/mmservice/MapMatcherController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017 the enviroCar community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.envirocar.processing.mapmatching.mmservice;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author dewall
+ */
+@RestController
+@RequestMapping("/mapmatching")
+public class MapMatcherController {
+
+    private final MapMatcherService mapMatcher;
+
+    /**
+     *
+     * @param mapMatcher
+     */
+    @Autowired
+    public MapMatcherController(MapMatcherService mapMatcher) {
+        this.mapMatcher = mapMatcher;
+    }
+
+    @RequestMapping(value = "/ec-matcher", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity matchEnvirocarTrack(HttpServletRequest request) {
+        return null;
+    }
+
+}
