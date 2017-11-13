21f7bdcf4073c782dcfefb28d8d031a11eb541cd
dewall <a.dewall@52north.org>
2017-11-13 09:55:50 +0100

    some minor changes to the mapserver build script

--- etc/mapserver/build.sh
+++ etc/mapserver/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+osm_dl_link="http://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf"
+
 docker_tag="ec-mapmatching/mapserver"
 
 mapserver_dbname="osmdata"
@@ -8,20 +10,23 @@ mapserver_user="osmuser"
 mapserver_pass="pass"
 mapserver_mode="slim"
 
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${scriptDir}" #&& mkdir data
+
 # build docker image
 docker build -t="ec-mapmatching/mapserver" .
 
 # run the docker image
 docker run -d \
-    -v /home/sertzu/dev/envirocar-ec4BIT/Docker/mapmatching/mapserver/data:/data \
+    -v $scriptDir/data:/data \
     --name mapserver \
     ec-mapmatching/mapserver 
 
 # wait a certain amount of time for the database to be initialized...
-sleep 15
+#sleep 15
 
 # execute the import stuff and commit the changes. 
 docker exec -it mapserver /bin/bash --rcfile /root/.bashrc -ci \
-    "/mnt/map/osm/import.sh /data/osm-data.osm.pbf osmdata osmuser pass /mnt/map/tools/road-types.json slim"
+    "wget -O /data/osm-data.osm.pbf $osm_dl_link && /mnt/map/osm/import.sh /data/osm-data.osm.pbf $mapserver_dbname $mapserver_user $mapserver_pass /mnt/map/tools/road-types.json $mapserver_mode"
 docker commit mapserver ec-mapmatching/mapserver
 docker stop mapserver && docker rm mapserver
